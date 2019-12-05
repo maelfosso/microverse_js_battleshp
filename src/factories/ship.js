98865757694526d4662686d78359dbec4862dfeb
Mael FOSSO <fosso.mael.elvis@gmail.com>
2019-12-05 22:40:22 +0100

    Ship is OK

--- src/factories/ship.js
+++ src/factories/ship.js
@@ -1,9 +1,18 @@
-const Ship = {
+const Ship = (length) => {
   length,
+  hits = new Array(length).fill(false),
 
-  hit = (num) => {
-    
+  hit = (index) => {
+    if (index < 0 && index > length) {
+      throw new Error('error ...');
+    }
+    this.hits[index] = true;
+  },
+
+  isSunk = () => {
+    return this.hits.every(true);
   }
+
 };
 
 export default Ship;
